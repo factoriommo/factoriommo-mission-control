b7a2a770b37ac460e6046db72e55d4db7edd86f6
Arnoud van Heuvelen <avanheuvelen@gmail.com>
2016-10-01 01:34:03 +0200

    Rocket launched event

--- factoriomc/aliensvstrees/scenario.py
+++ factoriomc/aliensvstrees/scenario.py
@@ -10,34 +10,14 @@ class Scenario(object):
         data_list = { }
 
         for server in servers:
-            try:
-                pack1 = ConsumptionStat.objects.filter(server=server.id) \
-                    .filter(key='science-pack-1').order_by('-id')[0].value
-            except IndexError:
-                pack1 = 0
-            try:
-                pack2 = ConsumptionStat.objects.filter(server=server.id) \
-                    .filter(key='science-pack-2').order_by('-id')[0].value
-            except IndexError:
-                pack2 = 0
-            try:
-                pack3 = ConsumptionStat.objects.filter(server=server.id) \
-                    .filter(key='science-pack-3').order_by('-id')[0].value
-            except IndexError:
-                pack3 = 0
-            try:
-                pack4 = ConsumptionStat.objects.filter(server=server.id) \
-                    .filter(key='alien-science-pack').order_by('-id')[0].value
-            except IndexError:
-                pack4 = 0
+            data_list[server.pk] = {'players-online': '0'}
 
-            data_list[server.pk] = {
-                'science-pack-1': pack1,
-                'science-pack-2': pack2,
-                'science-pack-3': pack3,
-                'alien-science-pack': pack4,
-                'players-online': '0',
-            }
+            for key in ['science-pack-1', 'science-pack-2', 'science-pack-3', 'alien-science-pack']:
+                try:
+                    data_list[server.pk][key] = ConsumptionStat.objects.filter(server=server.id) \
+                        .filter(key=key).order_by('-id')[0].value
+                except IndexError:
+                    data_list[server.pk][key] = 0
 
         pack = {'namespace': 'scores', 'data': data_list}
         for server in servers:
