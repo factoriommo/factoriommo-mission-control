d38137d2863098bfa4348b5fcb469a93401c5ac4
Arnoud van Heuvelen <avanheuvelen@gmail.com>
2016-10-01 00:47:55 +0200

    Exit cleanly

--- factoriomc/core/management/commands/run_scenario_one.py
+++ factoriomc/core/management/commands/run_scenario_one.py
@@ -11,41 +11,48 @@ class Command(BaseCommand):
     help = 'Run scenario one'
 
     def handle(self, *args, **options):
-        while True:
+        running=True
+
+        self.stdout.write("Starting scenario one..")
+        while running:
             reset_queries()
-            sleep(WAIT_TIME)
-            print(".")
-            # Send Production stats
-            servers = Server.objects.all()
+            try:
+                sleep(WAIT_TIME)
+                print(".")
+                # Send Production stats
+                servers = Server.objects.all()
 
-            data_list = { }
+                data_list = { }
 
-            for server in servers:
-                try:
-                    pack1 = ConsumptionStat.objects.filter(server=1).filter(key='science-pack-1').order_by('-id')[0]
-                except IndexError:
-                    pack1 = 0
-                try:
-                    pack2 = ConsumptionStat.objects.filter(server=1).filter(key='science-pack-2').order_by('-id')[0]
-                except IndexError:
-                    pack2 = 0
-                try:
-                    pack3 = ConsumptionStat.objects.filter(server=1).filter(key='science-pack-3').order_by('-id')[0]
-                except IndexError:
-                    pack3 = 0
-                try:
-                    pack4 = ConsumptionStat.objects.filter(server=1).filter(key='alien-science-pack').order_by('-id')[0]
-                except IndexError:
-                    pack4 = 0
+                for server in servers:
+                    try:
+                        pack1 = ConsumptionStat.objects.filter(server=1).filter(key='science-pack-1').order_by('-id')[0]
+                    except IndexError:
+                        pack1 = 0
+                    try:
+                        pack2 = ConsumptionStat.objects.filter(server=1).filter(key='science-pack-2').order_by('-id')[0]
+                    except IndexError:
+                        pack2 = 0
+                    try:
+                        pack3 = ConsumptionStat.objects.filter(server=1).filter(key='science-pack-3').order_by('-id')[0]
+                    except IndexError:
+                        pack3 = 0
+                    try:
+                        pack4 = ConsumptionStat.objects.filter(server=1).filter(key='alien-science-pack').order_by('-id')[0]
+                    except IndexError:
+                        pack4 = 0
 
-                data_list[server.pk] = {
-                    'science-pack-1': pack1,
-                    'science-pack-2': pack2,
-                    'science-pack-3': pack3,
-                    'alien-science-pack': pack4,
-                    'players-online': '0',
-                }
+                    data_list[server.pk] = {
+                        'science-pack-1': pack1,
+                        'science-pack-2': pack2,
+                        'science-pack-3': pack3,
+                        'alien-science-pack': pack4,
+                        'players-online': '0',
+                    }
 
-            pack = {'namespace': 'scores', 'data': data_list}
-            for server in servers:
-                server.message(pack)
+                pack = {'namespace': 'scores', 'data': data_list}
+                for server in servers:
+                    server.message(pack)
+            except KeyboardInterrupt:
+                self.stdout.write("Stopping..")
+                running=False
